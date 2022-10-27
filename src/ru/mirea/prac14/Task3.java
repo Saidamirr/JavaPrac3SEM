c806d9c594bc353e56a11efdfee1cbb2cfb097e2
JosukeBukin <imi200303@gmail.com>
2022-10-27 21:14:43 +0300

    prac14 format fixes

---
+++ src/ru/mirea/prac14/Task3.java
@@ -0,0 +1,17 @@
+package ru.mirea.prac14;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String regex = "\\d+(\\.\\d+)?\\s?((USD)|(RUB)|(EUR))";
+        String str = "This phone costs 100 USD, 90 EUR or 45000 RUB";
+
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(str);
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+    }
+}
---
+++ src/ru/mirea/prac14/Task4.java
@@ -0,0 +1,16 @@
+package ru.mirea.prac14;
+
+import java.util.regex.Pattern;
+
+public class Task4 {
+    public static boolean plusAfterNum(String str){
+        String regex = "\\d+\\s*\\+";
+        Pattern p = Pattern.compile(regex);
+        return p.matcher(str).find();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(plusAfterNum("(1 + 8) – 9 / 4"));
+        System.out.println(plusAfterNum("6 / 5 – 2 * 9 "));
+    }
+}
